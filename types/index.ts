355f7f5fbf1ceb0241ce2b8c2801fb5c86221101
kilelx <llx.kieran@gmail.com>
2024-06-26 15:12:03 +0200

    X

---
+++ types/index.ts
@@ -0,0 +1,10 @@
+import { MouseEventHandler } from "react";
+
+// An interface specifies how a specific structure should look like
+export interface customButtonProps {
+    title: string,
+    // It's optionnal
+    containerStyles?: string,
+    // A mouse event on a HTML button
+    handleClick?: MouseEventHandler<HTMLButtonElement>
+}
