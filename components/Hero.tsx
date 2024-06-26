be5aa6cba5efa32819a2f613969009362f770a24
kilelx <llx.kieran@gmail.com>
2024-06-26 15:18:37 +0200

    done: hero section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -23,6 +23,15 @@ const Hero = () => {
           handleClick={handleScroll}
         />
       </div>
+
+      <div className="hero__image-container">
+        <div className="hero__image">
+          <Image
+            src="/hero.png" alt="hero image" fill className="object-contain"
+          />
+          <div className="hero__image-overlay"/>
+        </div>
+      </div>
     </div>
   )
 }
