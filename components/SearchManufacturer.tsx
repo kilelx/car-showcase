de11f071d2bc7daf483092f110d71972f63933e1
kilelx <llx.kieran@gmail.com>
2024-06-28 14:47:41 +0200

    wip on combobox

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -1,11 +1,35 @@
+'use client'
+
+import { useState } from "react"
+import Image from "next/image"
 import { searchManufacturerProps } from "@/types"
-import { Combobox, Transition } from "@headlessui/react"
+import { Combobox, ComboboxButton, ComboboxInput, Transition } from "@headlessui/react"
 
 const SearchManufacturer = ({manufacturer, setManufacturer} : searchManufacturerProps) => {
+
+  const [query, setQuery] = useState('');
+
   return (
     <div className="search-manufacturer">
       <Combobox>
-        <div className="relative w-full"></div>
+        <div className="relative w-full">
+          <ComboboxButton className='absolute top-[14px]'>
+            <Image
+              src="/car-logo.svg"
+              width={20}
+              height={20}
+              className="ml-4"
+              alt="Car logo"
+            />
+          </ComboboxButton>
+          <ComboboxInput
+            className="search-manufacturer__input"
+            placeholder="Volkswagen"
+            displayValue={(manufacturer: string) => manufacturer}
+            // Updating the value
+            onChange={(e) => setQuery(e.target.value)}
+          />
+        </div>
       </Combobox>
     </div>
   )
